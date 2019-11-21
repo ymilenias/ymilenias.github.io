cf379bc7cc2c3e5548ae59f745aea2e700a300fa
ymilenias <yemimamileniaa@gmail.com>
2019-11-21 16:17:36 +0700

    hallo kaka aga

---
+++ precache-manifest.a1eb85f74b26796d6a5795a25ef7c26c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51393e2454f707e23a7",
+    "url": "/css/chunk-vendors.48fa860f.css"
+  },
+  {
+    "revision": "0f77ae0fd7acf3c9840b627c33eb9895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6115167818b0c5277983",
+    "url": "/js/about.d7ef9484.js"
+  },
+  {
+    "revision": "1c02d77067711e76f737",
+    "url": "/js/app.5ed5abd9.js"
+  },
+  {
+    "revision": "e51393e2454f707e23a7",
+    "url": "/js/chunk-vendors.3014de51.js"
+  },
+  {
+    "revision": "d8bae9bdd7fbf8721c7bf0bac903728d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
